8c9816895bbf9ea1e9c9ec8822bbaf9a3598d68e
Babin Bohora <bohorababin1@gmail.com>
2024-03-01 20:37:14 +0900

    Added more routes for Book: create, show, update & delete

--- server/routes/api/books.js
+++ server/routes/api/books.js
@@ -17,3 +17,41 @@ router.get("/", (req, res) => {
     .then(books => res.json(books))
     .catch(err => res.status(404).json({ nobooksfound: "No Books found" }))
 });
+
+// @route    GET api/books/:id
+// @desc     Get single book by :id
+// @access   Public
+router.get("/:id", (req, res) => {
+  Book.findById(req.params.id)
+    .then(book => res.json(book))
+    .catch(err => res.status(404).json({ nobookfound: "No Book found" }))
+});
+ 
+// @route    POST api/books
+// @desc     Add/save book
+// @access   Public
+router.post("/", (req, res) => {
+  Book.create(req.body)
+    .then(book => res.json({ msg: "Book successfully created" }))
+    .catch(err => res.status(400).json({ error: "Unable to add this book" }))
+});
+ 
+// @route    PUT api/books/:id
+// @desc     Update book by :id
+// @access   Public
+router.put("/:id", (req, res) => {
+  Book.update(req.body)
+    .then(book => res.json({ msg: "Book successfully updated" }))
+    .catch(err => res.status(400).json({ error: "Unable to update the Database" }))
+});
+
+// @route    DELETE api/books/:id
+// @desc     Delete book by :id
+// @access   Public
+router.delete("/:id", (req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+    .then(book => res.json({ msg: "Book entry deleted successfully" }))
+    .catch(err => res.status(404).json({ error: "No such book found" }))
+});
+
+module.exports = router 
