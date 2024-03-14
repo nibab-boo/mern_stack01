aeb57665a439452c4e5bb04f1e2e0b4b4b1943a4
Babin Bohora <bohorababin1@gmail.com>
2024-03-14 19:59:15 +0900

    Connect server to db and routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 require('dotenv').config();
 const express = require("express");
 const connectDB = require("./config/db");
+const routes = require("./routes/api/books");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+// use the cors middleware with the
+// origin and credential options
+app.use(cors({origin: true, credential: true}));
+
+// use the body-parser middleware to parson JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use the routes module as a middleware
+// for the /api/books path
+app.use("/api/books", routes);
+
 // Connect Database
-connectDB
+connectDB();
 
 const PORT = process.env.PORT || 8082;
 
